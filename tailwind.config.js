5374797429638b3599998f39f545e8587eac6e79
eliezer <eliezer.c.alves2015@gmail.com>
2022-03-10 22:09:12 -0300

    feat(layout): definindo tonaliadades da cor primaria

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {      
+      colors: {
+        'primary': {
+          '100': '#ADD2FF',
+          '200': '#70B0FF',
+          '300': '#3993FF',
+          '400': '#0A78FF',
+          '500': '#005CCC',
+          '600': '#00408F',
+          '700': '#002552',
+        }        
+      },
+    },
   },
   plugins: [],
 }
