420de66339928c099648a63c6bee4f6faab81fd2
eliezer <eliezer.c.alves2015@gmail.com>
2022-03-14 21:53:58 -0300

    feat(style): definindo cor secundaria / criando componente ThemeColors

---
+++ src/components/ThemeColors/index.tsx
@@ -0,0 +1,26 @@
+export function ThemeColors() {
+  return (
+    <div className="flex-center w-full gap-4">
+      <div className="flex-col-center w-1/3">
+        <h3>Primary</h3>
+        <div className="w-full h-7 bg-primary-50"></div>
+        <div className="w-full h-7 bg-primary-100"></div>
+        <div className="w-full h-7 bg-primary-200"></div>
+        <div className="w-full h-7 bg-primary-300"></div>
+        <div className="w-full h-7 bg-primary-400"></div>
+        <div className="w-full h-7 bg-primary-500"></div>
+        <div className="w-full h-7 bg-primary-600"></div>
+      </div>
+      <div className="flex-col-center w-1/3">
+      <h3>Secondary</h3>
+        <div className="w-full h-7 bg-secondary-50"></div>
+        <div className="w-full h-7 bg-secondary-100"></div>
+        <div className="w-full h-7 bg-secondary-200"></div>
+        <div className="w-full h-7 bg-secondary-300"></div>
+        <div className="w-full h-7 bg-secondary-400"></div>
+        <div className="w-full h-7 bg-secondary-500"></div>
+        <div className="w-full h-7 bg-secondary-600"></div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,18 @@
+import { ThemeColors } from "../../components/ThemeColors";
 
 export function Home() {
   return (
-    <div className="flex-center">
-      <h1>Scrum Poker para equipes de desenvolvimento ágil</h1>
-      <h2>Scrum Poker para equipes de desenvolvimento ágil</h2>
-      <h3>Scrum Poker para equipes de desenvolvimento ágil</h3>
-      <span>Ferramenta simples e envolvente para fazer estimativas.</span>
+    <div className="h-full flex-center">
+      <main className="w-1/2 h-2/3 flex-col-center gap-4">
+        <div className="flex flex-col">
+          <h1 className="font-semibold">Scrum Poker para equipes de desenvolvimento ágil</h1>
+          <h3>Ferramenta simples para fazer estimativas.</h3>
+        </div>
+        <button type="button" className="btn btn-primary w-full">Criar nova sala</button>
+        <span>ou</span>
+        <input type="text" placeholder="Entre em uma sala já existente com o código"/>
+        <ThemeColors/>
+      </main>
     </div>
   )
 }
