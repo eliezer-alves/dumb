22477a6f4318446c8519b9eeaedfa75bf0a84e61
eliezer <eliezer.c.alves2015@gmail.com>
2022-03-15 09:52:57 -0300

    feat(Home): primeira versao do layout da pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,14 +3,18 @@ import { ThemeColors } from "../../components/ThemeColors";
 export function Home() {
   return (
     <div className="h-full flex-center">
-      <main className="w-1/3 h-2/3 flex-col-center gap-2">
-        <div className="flex flex-col mb-4">
+      <main className="w-1/3 h-2/3 flex-col-center">
+        <div className="flex flex-col mb-8">
           <h1 className="font-semibold">Scrum Poker para equipes de desenvolvimento ágil</h1>
           <h3>Ferramenta simples para fazer estimativas.</h3>
         </div>
-        <button type="button" className="btn btn-primary w-full">Criar nova sala</button>
-        <span>ou</span>
-        <input type="text" placeholder="Entre em uma sala já existente com o código" className="w-full"/>
+
+        <button className="btn btn-primary w-full">Criar nova sala</button>
+        <div className="separator">ou entre em uma sala</div>
+        <div className="flex-center w-full gap-2">
+          <input type="text" placeholder="digite o código da sala" className="w-2/3 m-0"/>
+          <button className="btn btn-dark w-1/3 m-0">Entrar</button>
+        </div>
       </main>
     </div>
   )
