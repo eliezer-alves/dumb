e61b4140b7f946149b827a2c9b6b7a629f47e747
eliezer <eliezer.c.alves2015@gmail.com>
2022-03-15 07:56:00 -0300

    feat(style): finalizando estilizacao do input padrao

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,15 +3,14 @@ import { ThemeColors } from "../../components/ThemeColors";
 export function Home() {
   return (
     <div className="h-full flex-center">
-      <main className="w-1/2 h-2/3 flex-col-center gap-4">
-        <div className="flex flex-col">
+      <main className="w-1/3 h-2/3 flex-col-center gap-2">
+        <div className="flex flex-col mb-4">
           <h1 className="font-semibold">Scrum Poker para equipes de desenvolvimento ágil</h1>
           <h3>Ferramenta simples para fazer estimativas.</h3>
         </div>
         <button type="button" className="btn btn-primary w-full">Criar nova sala</button>
         <span>ou</span>
-        <input type="text" placeholder="Entre em uma sala já existente com o código"/>
-        <ThemeColors/>
+        <input type="text" placeholder="Entre em uma sala já existente com o código" className="w-full"/>
       </main>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,15 @@ module.exports = {
         }
       },
     },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    },
   },
   plugins: [],
 }
