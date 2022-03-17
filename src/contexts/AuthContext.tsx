eb04025c2ce9822a4435e56dfff301bd3b4e5820
eliezer <eliezer.c.alves2015@gmail.com>
2022-03-17 09:02:25 -0300

    feat(auth): criando contexto de autenticacao do sistema

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,76 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { firebase, auth } from '../services/firebase';
+
+type User = {
+	id: string;
+	name: string;
+	avatar: string;
+}
+
+type AuthContextType = {
+	user: User | undefined;
+	signInWithGoogle: () => void;
+	logout: () => void;
+}
+
+type AuthContextProviderProps = {
+	children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
+
+export function AuthContextProvider(props: AuthContextProviderProps) {
+	const [user, setUser] = useState<User>();
+
+	const handleSetUser = (firebaseUser: any) => {
+		const { displayName, photoURL, uid } = firebaseUser;
+
+		if (!displayName || !photoURL) {
+			throw new Error('Missing information from Google Account.');
+		}
+
+		setUser({
+			id: uid,
+			name: displayName,
+			avatar: photoURL
+		});
+	}
+
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged(user => {
+			if (user) {
+				handleSetUser(user)
+			}
+		})
+
+		return () => {
+			unsubscribe();
+		}
+	}, []);
+
+	async function signInWithGoogle() {
+		const provider = new firebase.auth.GoogleAuthProvider();
+
+		const result = await auth.signInWithPopup(provider)
+
+		if (result.user) {
+			handleSetUser(result.user)
+		}
+	}
+
+	async function logout() {		
+		if (window.confirm(`Tem certeza que não desej mais continuar como ${user?.name}?`)) {			
+			firebase.auth().signOut().catch(function(error) {
+				alert('Houve augum problema ao efetuar seu logout! Tente novamente.');
+			});
+		}
+
+		setUser(undefined);
+	}
+
+	return (
+		<AuthContext.Provider value={{ user, signInWithGoogle, logout }}>
+			{props.children}
+		</AuthContext.Provider>
+	);
+}
