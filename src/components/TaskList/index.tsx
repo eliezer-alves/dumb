f49ca2ca25c761753d0ff71d1fc350223ad115c3
eliezer <eliezer.c.alves2015@gmail.com>
2022-03-24 10:00:41 -0300

    feat(tasks): destacando tarefa que esta sendo votada

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,5 +1,6 @@
 import { useRoom } from "../../hooks/useRoom"
 import binIcon from './images/bin-icon.svg'
+import cx from 'classnames';
 
 export function TaskList() {
   const { tasks, taskVote, deleteTask, setTaskVote } = useRoom()
@@ -9,7 +10,10 @@ export function TaskList() {
       {
         tasks.map(task =>{
           return (
-            <div key={task.id} className="w-full p-4 pt-2 flex flex-col bg-gray-100 rounded-lg shadow-md">
+            <div key={task.id}
+              className={cx('w-full p-4 pt-2 flex flex-col bg-gray-100 rounded-lg shadow-md',
+                {'border-2 border-primary-200 bg-primary-50': (task.id == taskVote?.id)}
+              )}>
               <div onClick={() => deleteTask(task.id)} className="w-full flex items-center justify-end">
                 <img src={binIcon} width="42" alt="" className="p-2 link rounded-full hover:bg-gray-200" />
               </div>              
