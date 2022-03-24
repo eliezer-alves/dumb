6a81871bdd9339283920ee0f922341aefa25f821
eliezer <eliezer.c.alves2015@gmail.com>
2022-03-24 04:35:12 -0300

    feat(component - TaskList): criando componente para a listagem das tarefas

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,8 +1,26 @@
+import { useRoom } from "../../hooks/useRoom"
+import binIcon from './images/bin-icon.svg'
 
 export function TaskList() {
+  const { tasks } = useRoom()
   return (
-    <div>
-      
+    <div className="w-full flex-col-center gap-4">
+      {
+        tasks.map(task =>{
+          return (
+            <div className="w-full p-4 pt-2 flex flex-col bg-gray-100 rounded-lg shadow-md">
+              <div className="w-full flex items-center justify-end">
+                <img src={binIcon} width="42" alt="" className="p-2 link rounded-full hover:bg-gray-200" />
+              </div>              
+              <h3 className="mb-6">{task.title}</h3>
+              <div className="w-full flex items-center justify-between">
+                <button className="btn-sm btn-secondary text-sm font-sans w-24">votar</button>
+                <span className="btn-sm btn-secondary text-sm font-sans">0</span>
+              </div>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
--- src/components/TaskSideBar/index.tsx
+++ src/components/TaskSideBar/index.tsx
@@ -3,6 +3,7 @@ import NewTask from '../NewTask'
 import closeIcon from './images/close-icon.svg'
 import { Modal } from "../BaseModal"
 import { useRoom } from '../../hooks/useRoom'
+import { TaskList } from '../TaskList'
 
 export function TaskSideBar() {
   const { tasks } = useRoom()
@@ -13,8 +14,8 @@ export function TaskSideBar() {
   return (
     <Modal>
       <div className="w-full h-full flex justify-end">
-        <div className="h-full min-w-1/5 px-10 py-10 flex flex-col justify-start gap-4 bg-gray-50">
-          <div className="mb-4 flex items-center justify-between">
+        <div className="h-full min-w-1/5 flex flex-col justify-start gap-4 bg-gray-50">
+          <div className="h-1/12 p-10 flex items-center justify-between">
             <h2>Tarefas</h2>
             <img
               onClick={() => {setShowModal(false)}}
@@ -23,7 +24,12 @@ export function TaskSideBar() {
               alt="Fechar barra de tarefas"
               className="p-1 link rounded-full hover:bg-gray-200" />
           </div>
-          <NewTask/>
+          <div className="h-3/5 px-5 py-5 overflow-y-scroll">
+            <TaskList />
+          </div>
+          <div className="px-5">
+            <NewTask/>
+          </div>
         </div>
       </div>
     </Modal>
