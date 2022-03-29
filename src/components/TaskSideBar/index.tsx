adfe2d0bb74b87131d940a9b7926327ef3fd74b3
eliezer <eliezer.c.alves2015@gmail.com>
2022-03-29 10:05:20 -0300

    feat(TopBar): trabalhando responsividade menu superior

--- src/components/TaskSideBar/index.tsx
+++ src/components/TaskSideBar/index.tsx
@@ -13,7 +13,7 @@ export function TaskSideBar() {
     <Modal>
       <div className="modal w-full h-full flex justify-end">
         <div className="modalWindow h-full px-4 flex flex-col justify-start bg-gray-50">
-          <div className="h-1/12 p-10 flex items-center justify-between">
+          <div className="h-1/12 p-10 flex items-center justify-between mobile:px-0">
             <h2>Tarefas</h2>
             <img
               onClick={() => {setShowModal(false)}}
--- src/components/TopBar/MobileTopBar.tsx
+++ src/components/TopBar/MobileTopBar.tsx
@@ -1,6 +1,7 @@
 import { useRoom } from "../../hooks/useRoom";
 import { ButtonShowSideBar } from "../ButtonShowSideBar";
 import { ButtonShowTasks } from "../ButtonShowTasks";
+import { Logo } from "../Logo";
 
 export function MobileTopBar() {
   const { code, name } = useRoom();
@@ -8,7 +9,8 @@ export function MobileTopBar() {
   return (
     <div className="w-full h-1/6 p-4 flex justify-start">
       <div className="w-full h-14 flex justify-between items-center">
-        { code && <ButtonShowSideBar /> }
+        {!code && <Logo />}
+        { <ButtonShowSideBar /> }
         { name && <span>{name}</span> }
         { code && <ButtonShowTasks /> }
       </div>
