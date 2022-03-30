76ddae526af83a3141eb53731893edda5cd973c1
eliezer <eliezer.c.alves2015@gmail.com>
2022-03-30 07:47:50 -0300

    feat(Home - mobile): estilizando pagina inicial para mobile devices

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -7,11 +7,11 @@ export function Logo() {
 
   return (
     <div onClick={() => {navigate('/')}} className="flex-center gap-4 link">
-      <img src={millenniumIcon} alt="System incon" />
+      <img src={millenniumIcon} alt="System incon" className="mobile:w-[44px] h-[44px]"/>
       <div className="flex items-center gap-4">
         <div className="flex flex-col font-fredoka">
-          <span className="text-3xl font-bold text-primary-300">millennium</span>
-          <span className="text-xl font-semibold text-secondary-300">Planning Poker</span>
+          <span className="text-3xl font-bold text-primary-300 mobile:text-2xl">millennium</span>
+          <span className="text-xl font-semibold text-secondary-300 mobile:text-sm">Planning Poker</span>
         </div>
       </div>
     </div>
