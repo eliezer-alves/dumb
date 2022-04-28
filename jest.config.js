f7ebb8ab7ac0e45f7e044ae715136cef0e745330
eliezer <eliezer.c.alves2015@gmail.com>
2022-04-28 00:38:35 -0300

    chore: criando configs do jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+}
