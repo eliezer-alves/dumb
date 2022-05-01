0b34f8246b965d0fac2c6d0404783ac822e59740
eliezer <eliezer.c.alves2015@gmail.com>
2022-05-01 17:07:11 -0300

    test: [infra] lancando AccessDeniedError no FirebaseRemoteService quando PERMISSION_DENIED

--- src/tests/infra/http/firebase-remote-service.spec.ts
+++ src/tests/infra/http/firebase-remote-service.spec.ts
@@ -1,19 +1,33 @@
 import { HttpRequest } from "@/data/protocols/http"
 import { FirebaseRemoteService } from "@/infra/http/firebase-remote-service"
+import { AccessDeniedError } from "@/tests/domain/errors"
 import { mockCreateRoomParams } from "@/tests/domain/mocks"
 
 describe('FirebaseRemoteService', () => {
-  test('Ensures that the params in adapter FirebaseRemoteService is currect', async() => {
+  // test('Ensures that the params in adapter FirebaseRemoteService is currect', async() => {
+  //   const params: HttpRequest = {
+  //     url: 'rooms/',
+  //     method: 'post',
+  //     body: mockCreateRoomParams()
+  //   }
+  //   const sut = new FirebaseRemoteService()
+  //   const response = await sut.request(params)
+  //   console.log(response);
+    
+  //   // const mock = new MockFirebaseRemoteService()
+  //   expect({}).toEqual({})
+  // })
+
+  test('Should throw UnauthorizedError if adapter FirebaseRemoteService returns PERMISSION_DENIED', async() => {
     const params: HttpRequest = {
       url: 'rooms/',
       method: 'post',
       body: mockCreateRoomParams()
     }
     const sut = new FirebaseRemoteService()
-    const response = await sut.request(params)
-    console.log(response);
+    const promise = sut.request(params)
     
     // const mock = new MockFirebaseRemoteService()
-    expect({}).toEqual({})
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
