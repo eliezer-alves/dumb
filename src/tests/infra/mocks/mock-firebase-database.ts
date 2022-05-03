d6238192fc3a8071153cd15840c2382537ca9198
eliezer <eliezer.c.alves2015@gmail.com>
2022-05-03 16:32:32 -0300

    refactor: [test-infra] MockFirebaseDatabase - inserindo possibilidade de lancar Errors pelo mock

--- src/tests/infra/mocks/mock-firebase-database.ts
+++ src/tests/infra/mocks/mock-firebase-database.ts
@@ -5,13 +5,45 @@ export const mockHttpResponse = (): any => ({
   key: faker.random.alphaNumeric(10)
 })
 
+export enum FirebaseErrorCode {
+  PERMISSION_DENIED = 'PERMISSION_DENIED'
+}
+
+export class FirebaseError extends Error {
+  public code: string
+  constructor (code:string = '', message: string = 'Firebase Error') {
+    super(message)
+    this.name = 'FirebaseError'
+    this.code = code
+  }
+}
+
 export class MockFirebaseDatabase {
   private mockedFirebaseDatabase = firebaseDatabase as jest.Mocked<typeof firebaseDatabase>
+  private isError:boolean = false
+  private errorCode?:FirebaseErrorCode
+  private errorMessage?:string = 'Mocked Firebase Error'
 
   constructor () {}
 
   public mockPush(expectedResponse:any = mockHttpResponse) {
-    this.mockedFirebaseDatabase.push.mockClear().mockResolvedValue(expectedResponse)
+    console.log(this.isError)
+    if (this.isError) {
+      this.mockedFirebaseDatabase.push.mockClear().mockImplementation(() => {
+        throw new FirebaseError(this.errorCode, this.errorMessage)
+      })
+      console.log('here')
+      
+    } else {
+      this.mockedFirebaseDatabase.push.mockClear().mockResolvedValue(expectedResponse)
+    }
+
     return this.mockedFirebaseDatabase
+  }
+
+  public throwError(code:FirebaseErrorCode, message?: string):void {
+    this.isError = true
+    this.errorCode = code
+    this.errorMessage = message    
   }
 }
