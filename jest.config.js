7db9d15d48d50d11a61915aac13f3e9647dd315e
eliezer <eliezer.c.alves2015@gmail.com>
2022-05-07 13:56:28 -0300

    chore: configurando jest

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
-    '.+\\.(ts)$': 'ts-jest',
+    '.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
